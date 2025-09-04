ee8c7fef42c97fcb3b9d3e85282da78b8fe36d66
do-huu-ninh <ninhdh@vietis.com.vn>
2025-09-04 15:42:05 +0700

    Shop bán hàng init.

---
+++ shop/src/test/java/com/dhn/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhn.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
